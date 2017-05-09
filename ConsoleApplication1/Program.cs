3998d736a7091c3e3e86787da040606b3c226a47
Olaru Marcel <pompiliu_olaru@hotmail.com>
2017-05-09 12:51:06 +0200

    ArraysLeftRotation

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,12 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace ArraysLeftRotation
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Insert the number of integers in array (n) and the number of shifts (d)");
+           
+            string[] tokens_n = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(tokens_n[0]); //nb of elements in array
+            int d = Convert.ToInt32(tokens_n[1]); //nb of shifts 
+            
+
+            int[] a = new int[n];
+
+            Console.WriteLine($"Insert the {n} members of the array  ");
+            string[] a_temp = Console.ReadLine().Split(' ');
+            a = Array.ConvertAll(a_temp, int.Parse); //the array
+
+            int minshifts = d % a.Length;
+
+
+            if ((n >= 1) && (n <= Math.Pow(10, 5)) && ((d >= 1) && (d <= n)))
+            {
+                Console.WriteLine($"\n After {d} shifts the array is : \n");
+                for (int i = 0; i < minshifts; i++)
+                {
+                    Shift(a);
+                    
+                }
+
+                foreach (int i in a)
+                {
+                    
+                    Console.Write($"{i} ");
+                    
+                }
+                
+
+                Console.ReadKey();
+
+            }
+
+            else
+            {
+                Console.WriteLine("number of integers in array or the number of shifts is not in the right format ");
+            }
+
+
+
+        }
+
+        private static void Shift(int[] a)
+        {
+            int firstElement = a[0];
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                a[i] = a[i + 1];
+
+            }
+            a[a.Length - 1] = firstElement;
         }
     }
 }
